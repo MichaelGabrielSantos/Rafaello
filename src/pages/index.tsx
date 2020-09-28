d3c98af8524d89ba1121ff770dd2f36075349caf
Michael Santos <michaelgabrielsantos132@gmail.com>
2020-09-28 16:19:37 -0300

    Config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import Head from 'next/head'
 
-import MontsLogo from '../assets/images/logo.svg'
 import { Container } from '../styles/pages/home'
 
 const Home: React.FC = () => {
