83c5d005ad3df0bb3ce7f00255ccb18ad0c21dab
Michael Santos <michaelgabrielsantos132@gmail.com>
2020-09-28 16:21:43 -0300

    fix home.ts

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -3,8 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.main`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+
   min-height: 100vh;
   overflow: hidden;
 
